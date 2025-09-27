bc102279f10f254e7a7e7b063c3b2d956783aa97
renanjava <renancvr777@gmail.com>
2025-09-27 00:13:50 -0300

    feat: adding get data with fetch to render dynamic list

--- cookin-up-aula-1/src/components/ConteudoPrincipal.vue
+++ cookin-up-aula-1/src/components/ConteudoPrincipal.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue';
+
 export default {
     data() {
         return {
             ingredientes: ['Alho', 'Manteiga', 'Orégano']
         }
-    }
+    },
+    components: { SelecionarIngredientes }
 }
 </script>
 
@@ -26,6 +29,7 @@ export default {
                 Sua lista está vazia. selecione ingredientes para iniciar
             </p>
         </section>
+        <SelecionarIngredientes />
     </main>
 </template>
 
