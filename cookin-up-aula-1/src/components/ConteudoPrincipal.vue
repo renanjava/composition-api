8d5e435480fef68eec77ffdc6d34e40b2ae13dc7
renanjava <renancvr777@gmail.com>
2025-09-28 13:15:38 -0300

    feat: component SuaLista

--- cookin-up-aula-1/src/components/ConteudoPrincipal.vue
+++ cookin-up-aula-1/src/components/ConteudoPrincipal.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
-import Tag from './Tag.vue';
+import SuaLista from './SuaLista.vue';
 
 export default {
     data() {
@@ -8,28 +8,13 @@ export default {
             ingredientes: ['Alho', 'Manteiga', 'Orégano']
         }
     },
-    components: { SelecionarIngredientes, Tag }
+    components: { SelecionarIngredientes, SuaLista }
 }
 </script>
 
 <template>
     <main class="conteudo-principal">
-        <section>
-            <span class="subtitulo-lg sua-lista-texto">
-                Sua lista:
-            </span>
-
-            <ul v-if="ingredientes.length > 0" class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" v-bind:key="ingrediente">
-                    <Tag :texto="ingrediente" :ativa="true"/>
-                </li>
-            </ul>
-
-            <p v-else class="paragrafo lista-vazia">
-                <img src="../assets/imagens/icones/lista-vazia.svg" alt="ícone de pesquisa">
-                Sua lista está vazia. selecione ingredientes para iniciar
-            </p>
-        </section>
+        <SuaLista :ingredientes="ingredientes"/>
         <SelecionarIngredientes />
     </main>
 </template>
@@ -45,31 +30,6 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 5rem;
-}
-
-.sua-lista-texto {
-  color: var(--coral, #F0633C);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #F0633C);
-  text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {
