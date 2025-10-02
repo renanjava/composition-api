1565d10446fd429317cdd0851fc49c47c9b80a43
Renan <renancvr777@gmail.com>
2025-10-02 20:17:40 -0300

    feat: adding start button and run time in seconds

--- alura-tracker/src/components/Formulario.vue
+++ alura-tracker/src/components/Formulario.vue
@@ -1,36 +1,69 @@
 <template>
-    <div class="box">
-        <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?"/>
-                <div class="column">
-                    <div class="is-flex is-align-items-center is-justify-content-space-between">
-                        <section>
-                            <strong>00:00:00</strong>
-                        </section>
-                        <button class="button">
-                            <span class="icon">
-                                <i class="fas fa-play"></i>
-                            </span>
-                            <span>play</span>
-                        </button>
-                        <button class="button">
-                            <span class="icon">
-                                <i class="fas fa-stop"></i>
-                            </span>
-                            <span>stop</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+        <div class="column">
+          <div
+            class="is-flex is-align-items-center is-justify-content-space-between"
+          >
+            <section>
+              <strong>{{ tempoDecorrido }}</strong>
+            </section>
+            <button class="button" @click="iniciar()">
+              <span class="icon">
+                <i class="fas fa-play"></i>
+              </span>
+              <span>play</span>
+            </button>
+            <button class="button" @click="finalizar()">
+              <span class="icon">
+                <i class="fas fa-stop"></i>
+              </span>
+              <span>stop</span>
+            </button>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'Formulario',
-})
+  name: "Formulario",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000)
+        .toISOString()
+        .substr(11, 8);
+    },
+  },
+  methods: {
+    iniciar() {
+      setInterval(() => {
+        this.tempoEmSegundos += 1;
+        console.log(this.tempoEmSegundos);
+      }, 1000);
+    },
+    finalizar() {
+      console.log("finalizando");
+    },
+  },
+});
 </script>
