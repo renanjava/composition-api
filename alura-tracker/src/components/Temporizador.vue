0f9bc9da1c6e37b4f80476ebaa5d9bb294452223
Renan <renancvr777@gmail.com>
2025-10-03 00:07:14 -0300

    fix: stop button and play button can block itself

--- alura-tracker/src/components/Temporizador.vue
+++ alura-tracker/src/components/Temporizador.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempo-em-segundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar()">
+    <button class="button" @click="iniciar()" :disabled="cronometroRodando">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="finalizar()">
+    <button class="button" @click="finalizar()" :disabled="!cronometroRodando">
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -25,16 +25,18 @@ export default defineComponent({
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
+      cronometroRodando: false,
     };
   },
   components: {
-    Cronometro
+    Cronometro,
   },
   methods: {
     iniciar() {
       this.cronometro = setInterval(() => {
         this.tempoEmSegundos += 1;
       }, 1000);
+      this.cronometroRodando = true;
     },
     finalizar() {
       clearInterval(this.cronometro);
@@ -42,6 +44,7 @@ export default defineComponent({
         cronometro: this.cronometro,
         tempoEmSegundos: this.tempoEmSegundos,
       });
+      this.cronometroRodando = false;
     },
   },
 });
