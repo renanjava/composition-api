2a8e7b455e845dc0b27b2d91c6e90955afc0729f
Renan <renancvr777@gmail.com>
2025-10-02 23:43:46 -0300

    refactor: extract temporizador into component

--- alura-tracker/src/components/Formulario.vue
+++ alura-tracker/src/components/Formulario.vue
@@ -11,25 +11,8 @@
           class="input"
           placeholder="Qual tarefa você deseja iniciar?"
         />
-        <div class="column">
-          <div
-            class="is-flex is-align-items-center is-justify-content-space-between"
-          >
-            <Cronometro :tempo-em-segundos="tempoEmSegundos" />
-            <button class="button" @click="iniciar()">
-              <span class="icon">
-                <i class="fas fa-play"></i>
-              </span>
-              <span>play</span>
-            </button>
-            <button class="button" @click="finalizar()">
-              <span class="icon">
-                <i class="fas fa-stop"></i>
-              </span>
-              <span>stop</span>
-            </button>
-          </div>
-        </div>
+        <div class="column"></div>
+        <Temporizador />
       </div>
     </div>
   </div>
@@ -37,33 +20,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Cronometro from "./Cronometro.vue";
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
   name: "Formulario",
   components: {
-    Cronometro,
-  },
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0,
-    };
-  },
-
-  methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1;
-      }, 1000);
-    },
-    finalizar() {
-      clearInterval(this.cronometro);
-      console.log({
-        cronometro: this.cronometro,
-        tempoEmSegundos: this.tempoEmSegundos,
-      });
-    },
+    Temporizador,
   },
 });
 </script>
---
+++ alura-tracker/src/components/Temporizador.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempo-em-segundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar()">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar()">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+    };
+  },
+  components: {
+    Cronometro
+  },
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      clearInterval(this.cronometro);
+      console.log({
+        cronometro: this.cronometro,
+        tempoEmSegundos: this.tempoEmSegundos,
+      });
+    },
+  },
+});
+</script>
