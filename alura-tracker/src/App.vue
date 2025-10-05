2f81e5b15f78f04351699b3783e326952aeaa3b6
Renan <renancvr777@gmail.com>
2025-10-04 22:47:47 -0300

    feat: adding Tarefa component inside App component

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css"
+  />
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
       <BarraLateral />
     </div>
     <div class="column is-three-quarter">
       <Formulario />
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
+import Tarefa from "./components/Tarefa.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    BarraLateral, Formulario
-  }
-})
+    BarraLateral,
+    Formulario,
+    Tarefa,
+  },
+});
 </script>
 
-<style>
+<style scoped>
+.lista {
+  padding: 1.25rem;
+}
+.box {
+  background: #242423;
+}
 </style>
---
+++ alura-tracker/src/components/Tarefa.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">Descricao da Tarefa</div>
+      <div class="column">
+        <Cronometro />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "Tarefa",
+  components: {
+    Cronometro,
+  },
+});
+</script>
